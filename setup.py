a50dd8535efca32cd44ed4018440b3efe871ddbd
Mitch <184176373+BigMitchGit@users.noreply.github.com>
2025-08-23 01:29:35 +0200

    chore: update version number to 1.0.0

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="mt5_remote",
     python_requires=">=3.11",
-    packages=find_packages(
-        include=["mt5_remote", "mt5remote", "mt5_remote.*"]
-    ),
-    version="0.2.0",
+    packages=find_packages(include=["mt5_remote", "mt5remote", "mt5_remote.*"]),
+    version="1.0.0",
     description="MetaTrader5 with remote access using a client/server architecture",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
