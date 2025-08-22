7cdf7c9df497f437897dd58ffae2f549c9b70914
Mitch <184176373+BigMitchGit@users.noreply.github.com>
2025-08-22 23:32:57 +0200

    chore: update setup.py package configuration

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name="mt5-remote",
     python_requires=">=3.11",
-    packages=find_packages(include=["mt5-remote"]),
+    packages=find_packages(
+        include=["mt5-remote", "mt5remote", "mt5_remote", "mt5-remote"]
+    ),
     version="0.2.0",
-    description="MetaTrader5 with remote access with a client/server architecture",
+    description="MetaTrader5 with remote access using a client/server architecture",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     author="BigMitchGit",
